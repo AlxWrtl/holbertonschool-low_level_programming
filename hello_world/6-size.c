d5914009ad7644ca4e0c659dd3bf2a6876dae0ca
SuperPims <indexes-benzine0p@icloud.com>
2023-10-11 11:24:37 +0200

    size of various types

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("char: %lu\n", sizeof(char));
+printf("int: %lu\n", sizeof(int));
+printf("long int: %lu\n", sizeof(long int));
+printf("long long int: %lu\n", sizeof(long long int));
+printf("float: %lu\n", sizeof(float));
+
+return (0);
+}
