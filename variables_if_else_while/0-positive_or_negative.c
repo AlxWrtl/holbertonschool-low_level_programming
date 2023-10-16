e80974b7eb6a9fa380a76f129a710fec1727641b
SuperPims <indexes-benzine0p@icloud.com>
2023-10-16 17:17:53 +0200

    print numbers

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
+
 
 /* betty style doc for function main goes there */
 int main(void)
@@ -10,12 +12,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-   if n>0 
-		printf ("is positive\n");	
-	if n=0
-		printf ("is zero\n");
-	if n<0
-		pringtf ("is negative\n")
+   if (n>0) 
+		printf ("is positive\n", n);	
+	if (n=0)
+		printf ("is zero\n", n);
+	if (n<0)
+		printf ("is negative\n", n);
 		
-	return (0);
+return (0);
 }
