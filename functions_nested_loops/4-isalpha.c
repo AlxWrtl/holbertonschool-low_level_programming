cc1f36de1a2362cbd3aae702341515b56bc06923
SuperPims <indexes-benzine0p@icloud.com>
2023-10-17 20:10:59 +0200

    checks for alphabetic character

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *_insalpha - checks for for alphabetic character.
+ * @c: The character to check for the lower charactere
+ *
+ * Return: 1 if c is lowercase 0 otherwise. 
+ */
+
+int _isalpha(int c)
+{
+
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+
+return (1);
+return (0);
+}
+
