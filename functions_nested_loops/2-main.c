e8e1ff17545b9c9969cfa1b69b27a9d0010afe96
SuperPims <indexes-benzine0p@icloud.com>
2023-10-17 16:00:21 +0200

    alphabet_x10

---
+++ functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
