44b4237ae2743c0456e57856d3df06bd7a14b469
SuperPims <indexes-benzine0p@icloud.com>
2023-10-17 15:53:12 +0200

    alphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Print the lowercase alphabet to the console.
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_alphabet_x10(void)
+{
+char letter;
+int compteur;
+
+for (compteur = 0; compteur < 10; compteur++ )
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+
+
+{
+	_putchar(letter);
+
+}
+
+
+}
+
+_putchar('\n');
+
+}
+
