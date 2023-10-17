188e0ca3ad79ccddbf0144220481bd1224fc7221
SuperPims <indexes-benzine0p@icloud.com>
2023-10-17 20:58:32 +0200

    prints the sign of a number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *print_sign - checks for for alphabetic character.
+ * @n:  the sign of a number.
+ *
+ * Return: 1 if c is lowercase 0 otherwise,
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
+
+int print_sign(int n)
+{
+
+if (n > '0')
+{
+	_putchar('+');
+	return (1);
+}
+else if (n == '0')
+{
+	_putchar('0');
+	return (0);
+}
+else
+{
+	_putchar('-');
+	return (-1);
+}
+}
+
