439e84a37875db94b7b53ea48f54be1da3cf3052
SuperPims <indexes-benzine0p@icloud.com>
2023-10-18 21:33:44 +0200

    multiplication tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -17,28 +17,25 @@ void times_table(void)
 
 	for (multi = 0; multi <= 9; multi++)
 	{
-_putchar('0');
+		_putchar('0');
 
 		for (nbr = 1; nbr <= 9; nbr++)
 
 		{
 
 			result = multi * nbr;
-		
-		
-	_putchar(',');
-	_putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 
 			if (result <= 9)
 			{
-				_putchar(result +'0');
+				_putchar(' ');
+				_putchar(result + '0');
 			}
 			else
 			{
-				_putchar (result / 10 + '0');
-				_putchar (result % 10 + '0');
-
-
+				_putchar ((result / 10) + '0');
+				_putchar ((result % 10) + '0');
 			}
 		}
 		_putchar ('\n');
