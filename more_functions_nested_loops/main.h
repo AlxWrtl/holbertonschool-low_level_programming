92ceb08cc5d56db893a0aec7c1b77a0e14df9010
SuperPims <indexes-benzine0p@icloud.com>
2023-10-19 18:45:00 +0200

    checks for uppercase character

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+int _putchar(char c);
+
+void print_most_numbers(void);
+#endif
