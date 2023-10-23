2d7db36d19f1c26f2ad946d2759c6ed6bf67f004
SuperPims <indexes-benzine0p@icloud.com>
2023-10-23 11:18:33 +0200

    swaps the values

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - function that swap two numbers.
+ * @a: first num.
+ * @b: secong num
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
