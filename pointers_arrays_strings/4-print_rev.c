7ee42697526f4bb7ddb1811bd3266cef833d496f
SuperPims <indexes-benzine0p@icloud.com>
2023-10-23 21:13:09 +0200

    prints a string, in reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -12,11 +12,10 @@ void print_rev(char *s)
 	int text, save;
 
 	for (save = 0; s[save]; save++)
-	{	}
-
-		for (text = save; text + '\1'; text--)
+	{}
+		for (text = save; text + '\0'; text--)
 		{
 			_putchar(s[text]);
-		}
-		_putchar('\n');
+	}
+	_putchar('\n');
 }
