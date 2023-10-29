b7da9223669fa43b69b2b938ca296e618dc33e14
SuperPims <indexes-benzine0p@icloud.com>
2023-10-29 18:34:28 +0100

    encodes a string into 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337. 
+ * @s: string to be encoded
+ * Return: the resulting string;
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char letr[] = "aeotlAEOTL";
+	char numb[] = "4307143071";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letr[j] != '\0'; j++)
+		{
+			if (str[i] == letr[j])
+				str[i] = numb[j];
+		}
+	}
+	return (str);
+}
