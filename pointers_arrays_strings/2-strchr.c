3d541b6dec2727f38bf0af9cfbd940997e52b631
SuperPims <indexes-benzine0p@icloud.com>
2023-10-29 20:35:51 +0100

    locates a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string to search in 
+ * @c: char to find
+ *
+ * Return: Return if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
