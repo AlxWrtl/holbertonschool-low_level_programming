9a47de94b15d7f4375f35d6b25b00ab5ca6f6c65
SuperPims <indexes-benzine0p@icloud.com>
2023-11-02 13:45:53 +0100

    argc, argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * @argc: variable
+ * @argv: char
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[]) 
+{
+    if (argc > 0) 
+	{
+        printf("%s\n", argv[0]);
+    }
+
+    return (0);
+}
