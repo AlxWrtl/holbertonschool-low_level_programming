e44405118a2f7af84127863637add09074dbe2dc
SuperPims <indexes-benzine0p@icloud.com>
2023-11-02 19:35:23 +0100

    C - argc, argv

---
+++ argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - function
+ *
+ * @argc: variable
+ * @argv: String
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i, num;
+
+	for (i = 1; i < argc; i++)
+	{
+		num = atoi(argv[i]);
+
+		if (num <= 0)
+		{
+			if (argv[i][0] != '0' || argv[i][1] != '\0')
+			{
+				printf("Error\n");
+return (1);
+			}
+		}
+
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
