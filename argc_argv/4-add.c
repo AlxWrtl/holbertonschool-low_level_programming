62850809fb3e614127d3c2999ce3ccd882dea0be
SuperPims <indexes-benzine0p@icloud.com>
2023-11-02 19:39:40 +0100

    C - argc, argv

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -20,14 +20,12 @@ int main(int argc, char *argv[])
 	{
 		num = atoi(argv[i]);
 
-		if (num <= 0)
+		if(num <= 0) 
 		{
-			if(num <= 0) 
-			{
-				printf("Error\n");
-return (1);
-			}
+			printf("Error\n");
+		return (1);
 		}
+		
 
 		sum += num;
 	}
