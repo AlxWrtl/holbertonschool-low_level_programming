a05b9871155ce975d71780cfefdcc5e1110b316b
SuperPims <indexes-benzine0p@icloud.com>
2023-11-02 18:02:18 +0100

    prints its name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,14 +4,20 @@
  * main - function
  *
  * @argc: variable
- * @argv: char
+ * @argv: String
  * Return: 0
  */
 
 
-int main(int argc, char *argv[])
-{
-	(void)argv;
-	printf("%d\n", argc - 1);
-	return (0);
+void printProgramName(int argc, char *argv[]) {
+    if (argc > 0 && argv[0] != NULL) {
+        printf("%s\n", argv[0]);
+    } else {
+        fprintf(stderr, "Error: Unable to retrieve program name.\n");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    printProgramName(argc, argv);
+    return 0;
 }
