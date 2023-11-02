7fffe50727891214b593479587cacc9227619468
SuperPims <indexes-benzine0p@icloud.com>
2023-11-02 18:26:41 +0100

    C - argc, argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * printProgramName - function
+ * main - function
  *
  * @argc: variable
  * @argv: String
@@ -9,28 +9,17 @@
  */
 
 
-void printProgramName(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0 && argv[0] != NULL)
 	{
 		printf("%s\n", argv[0]);
 	}
+
 	else
 	{
-		fprintf(stderr, "Error: Unable to retrieve program name.\n");
+		return (1);
 	}
-}
-
-/**
- * main - function
- * @argc: variable
- * @argv: String
- * Return: 0
- */
-
-int main(int argc, char *argv[])
-{
-	printProgramName(argc, argv);
 
 	return (0);
 }
