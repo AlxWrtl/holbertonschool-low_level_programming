adff1ae8d78171ad56d4171e5520be7273204627
SuperPims <indexes-benzine0p@icloud.com>
2023-11-02 19:19:07 +0100

    C - argc, argv

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -15,19 +15,21 @@ int main(int argc, char *argv[])
 {
 
 	int num1, num2, result;
-    if (argc != 3)
+
+	if (argc != 3)
 	{
-        printf("Error\n");
-        return 1;
-    }
+		printf("Error\n");
 
-    num1 = atoi(argv[1]);
+	return (1);
+	}
 
-    num2 = atoi(argv[2]);
+	num1 = atoi(argv[1]);
 
-    result = num1 * num2;
-    
-    printf("%d\n", result);
+	num2 = atoi(argv[2]);
 
-    return 0;
+	result = num1 * num2;
+
+		printf("%d\n", result);
+
+	return (0);
 }
