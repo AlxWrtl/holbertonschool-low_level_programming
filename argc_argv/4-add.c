a16ec8d37ea1e21d1486516b18ab8b181f2285f0
SuperPims <indexes-benzine0p@icloud.com>
2023-11-03 15:00:28 +0100

    adds positive numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -20,12 +20,17 @@ int main(int argc, char *argv[])
 	{
 		num = atoi(argv[i]);
 
-		if(num <= 0) 
+		if (num == 0 && argv[i][0] != '0')
 		{
 			printf("Error\n");
 		return (1);
 		}
-		
+
+		if (num <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
 
 		sum += num;
 	}
