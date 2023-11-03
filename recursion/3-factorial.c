2f6782bda95665413d9e148e3970051702de46db
SuperPims <indexes-benzine0p@icloud.com>
2023-11-03 14:45:55 +0100

    returns the factorial of a given number

---
+++ recursion/3-factorial.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * factorial - prototypes
+ *
+ * @n: number
+ *
+ * Return: 0 or -1
+ */
+
+int factorial(int n)
+{
+
+	if (n < 0)
+	{
+	return (-1);
+	}
+
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+
+
+}
