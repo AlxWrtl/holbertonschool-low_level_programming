df3c256252b7474b121a87c960771b48755e04f7
SuperPims <indexes-benzine0p@icloud.com>
2023-11-09 22:28:30 +0100

    concatenates two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,9 +19,14 @@ char *str_concat(char *s1, char *s2)
 {
 	char *concatenated;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 
 	concatenated = malloc(strlen(s1) + strlen(s2) * sizeof(char) + 1);
