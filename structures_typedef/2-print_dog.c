a9eff58c00bf944b1b298767270833d8c10d8c40
SuperPims <indexes-benzine0p@icloud.com>
2023-11-15 10:09:36 +0100

    Prints information about a struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 		return;
 	}
 
-	printf("Name: %s\n", (d->name == NULL) ? "nil" : d->name);
+	printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", (d->owner == NULL) ? "nil" : d->owner);
+	printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
 }
