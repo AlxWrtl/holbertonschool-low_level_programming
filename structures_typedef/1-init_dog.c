00059e7a4fb9ec49585a687e23ed7253a75af3f2
SuperPims <indexes-benzine0p@icloud.com>
2023-11-15 09:49:38 +0100

    Initializes a struct dog with specified values

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -14,9 +14,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = strdup(name);
+	d->name = (name);
 
 	(*d).age = age;
 
-	d->owner = strdup(owner);
+	d->owner = (owner);
 }
