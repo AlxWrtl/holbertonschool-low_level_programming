e404b229bff8d240f131a0a6657f6a3c7aa36352
SuperPims <indexes-benzine0p@icloud.com>
2023-11-15 09:20:48 +0100

    Initializes a struct dog with specified values

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializes a struct dog with specified values.
+ * @d: Pointer to the struct dog to be initialized.
+ * @name: Pointer to a string for the name of the dog.
+ * @age: Float representing the age of the dog.
+ * @owner: Pointer to a string for the owner of the dog.
+ *
+ * This function initializes the members of the struct dog pointed to by 'd'
+ * with the provided values for name, age, and owner. Memory is allocated for
+ * name and owner, and the values are duplicated using strdup().
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = strdup(name);
+
+	(*d).age = age;
+
+	d->owner = strdup(owner);
+}
