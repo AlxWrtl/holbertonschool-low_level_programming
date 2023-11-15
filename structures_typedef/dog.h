3fe846ca5bed1e374f006a156ae15c9bf0580bf2
SuperPims <indexes-benzine0p@icloud.com>
2023-11-15 09:09:10 +0100

    Structure representing a dog with name, age, and owner

---
+++ structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct dog - Structure representing a dog with name, age, and owner.
+ * @name: Pointer to a string representing the name of the dog.
+ * @age: Float representing the age of the dog.
+ * @owner: Pointer to a string representing the owner of the dog.
+ */
+
+typedef struct dog
+{
+
+char *name;
+float age;
+char *owner;
+
+}dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+#endif
