8a904cdcd40ab706e585db2bbeef89eccb6eb8b7
SuperPims <indexes-benzine0p@icloud.com>
2023-11-15 13:47:32 +0100

    declare fonctions in the header

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef VARIA_FUNC
+#define VARIA_FUNC
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+
+#endif
