f4d3376572008796686fa82fccab2834aab80d22
SuperPims <indexes-benzine0p@icloud.com>
2023-11-16 15:33:32 +0100

    Implement print_name function with function pointer callback

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,15 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - Executes a function to print a name.
+ * @name: The name to be printed.
+ * @f: Pointer to a function that prints a name.
+ *
+ * This function takes a pointer to a function (@f) that prints a name and
+ * a string (@name) to be printed. If either the function pointer or the name
+ * is NULL, the function returns without performing any action.
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 
