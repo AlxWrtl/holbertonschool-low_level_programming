c9266abd7b352cb6ef630dde181ee28ca8befd9b
SuperPims <indexes-benzine0p@icloud.com>
2023-11-17 13:14:18 +0100

    Implement print_all function for formatted output

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,52 +1,49 @@
+
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
 
-void print_char(va_list data_list) {
-    printf("%c", va_arg(data_list, int));
-}
+/**
+ * print_all - prints anything based on the format provided
+ * @format: a list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int index = 0;
+	char *strg;
 
-void print_int(va_list data_list) {
-    printf("%d", va_arg(data_list, int));
-}
+	va_start(args, format);
 
-void print_float(va_list data_list) {
-    printf("%f", va_arg(data_list, double));
-}
+	while (format && format[index])
+	{
+		if (index)
+		{
+			printf(", ");
+		}
 
-void print_string(va_list data_list) {
-    char *string = va_arg(data_list, char *);
-    printf("%s", string ? string : "(nil)");
-}
+		switch (format[index++])
+		{
+			case 'c':
+			printf("%c", va_arg(args, int));
+			break;
 
-void print_all(const char * const format, ...) {
-    va_list data_list;
-    va_start(data_list, format);
+			case 'i':
+			printf("%d", va_arg(args, int));
+			break;
 
-    const char *separator = "";
+			case 'f':
+			printf("%f", (float)va_arg(args, double));
+			break;
 
-    for (int i = 0; format && format[i]; ++i) {
-        printf("%s", separator);
-        switch (format[i]) {
-            case 'c':
-                print_char(data_list);
-                break;
-            case 'i':
-                print_int(data_list);
-                break;
-            case 'f':
-                print_float(data_list);
-                break;
-            case 's':
-                print_string(data_list);
-                break;
-            default:
-                // Handle unsupported format specifiers if needed
-                break;
-        }
-        separator = ", ";
-    }
+			case 's':
+			strg = va_arg(args, char*);
+			printf("%s", strg ? strg : "(nil)");
+			break;
+		}
 
-    va_end(data_list);
-    printf("\n");
+	}
+	printf("\n");
+
+	va_end(args);
 }
