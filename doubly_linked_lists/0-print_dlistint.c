5af8a9b59ef904ab7a197e50941eb1569a0f42fd
SuperPims <indexes-benzine0p@icloud.com>
2023-11-30 16:25:54 +0100

    change betty comment

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -4,8 +4,10 @@
  * print_dlistint - Prints the elements of a doubly linked list.
  * @h: Pointer to the head of the doubly linked list.
  *
- * This function prints the integer value of each node in the doubly linked list
- * starting from the provided head. It returns the total number of nodes printed.
+ * This function prints the integer value of each node in the doubly
+ * linked list
+ * starting from the provided head. It returns the total number of
+ * nodes printed.
  *
  * Return: The number of nodes printed.
  */
@@ -21,5 +23,5 @@ size_t print_dlistint(const dlistint_t *h)
 		count++;
 	}
 
-	return count;
+	return (count);
 }
