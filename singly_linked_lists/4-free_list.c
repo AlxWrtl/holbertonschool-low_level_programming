62f6a3875841c745ae4f642b60540e491cf7d113
SuperPims <indexes-benzine0p@icloud.com>
2023-11-30 15:37:39 +0100

    Implement free_list function for deallocating linked list memory

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees memory allocated for a linked list.
+ * @head: Pointer to the head of the linked list.
+ *
+ * This function frees the memory allocated for each node in the linked list,
+ * including the strings within each node. It iterates through the list,
+ * freeing each node and its associated memory. Additionally, the system call
+ * for valgrind is included, which is not typically part of the free_list
+ * function.
+ */
+
+void free_list(list_t *head)
+{
+    list_t *temp;
+
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp->str);
+        free(temp);
+    }
+    system("valgrind --leak-check=full ./e");
+}
