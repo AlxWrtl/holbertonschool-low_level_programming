e08b84493de53ddadd387675e965d1a4a89854b8
SuperPims <indexes-benzine0p@icloud.com>
2023-11-30 15:35:13 +0100

    Implement list_len function for linked list length calculation

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - Computes the number of elements in a linked list.
+ * @h: Pointer to the head of the linked list.
+ *
+ * This function calculates and returns the number of elements in the linked
+ * list by iterating through the nodes until reaching the end.
+ *
+ * Return: The number of elements in the linked list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	for(;h != NULL; len++)
+	{
+		h = h->next;
+	}
+	return (len);
+}
