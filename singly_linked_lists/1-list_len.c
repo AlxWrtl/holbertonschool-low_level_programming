9382fa82e46cdeeac9275b8210e82df7e4bd5d5f
SuperPims <indexes-benzine0p@icloud.com>
2023-11-30 15:49:08 +0100

    betty space needed

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -4,7 +4,7 @@
  * list_len - Computes the number of elements in a linked list.
  * @h: Pointer to the head of the linked list.
  *
- * This function calculates and returns the number of elements in the linked
+ * This function calculates and returns the number of elements in the linked 1
  * list by iterating through the nodes until reaching the end.
  *
  * Return: The number of elements in the linked list.
@@ -14,7 +14,7 @@ size_t list_len(const list_t *h)
 {
 	size_t len = 0;
 
-	for(;h != NULL; len++)
+	for (; h != NULL; len++)
 	{
 		h = h->next;
 	}
