ea4c43d8a8cfc2895fdcd6275e9b098abcc2dede
SuperPims <indexes-benzine0p@icloud.com>
2023-12-06 14:19:28 +0100

    change the place of the close and return

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -47,11 +47,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	free(buffer);
-	close(fd);
 
 	if (bytes_written == -1 || (size_t)bytes_written < letters)
-		return (0);
+
+		free(buffer);
+
+	close(fd);
+	return (0);
 
 	return (bytes_read);
 }
