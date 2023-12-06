9cbf3b1083e7fcc015d562d6c4cd97663f09cc60
SuperPims <indexes-benzine0p@icloud.com>
2023-12-06 11:18:41 +0100

    betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,8 +6,10 @@
  * @text_content: Text content to write to the file.
  *
  * This function creates a file with the given filename and writes the provided
- * text content to it. If the file already exists, it is truncated. If text_content
- * is NULL, an empty file is created. The function returns 1 on success and -1 on failure.
+ * text content to it. If the file already exists, it is truncated.
+ * If text_content
+ * is NULL, an empty file is created. The function returns 1 on success
+ * and -1 on failure.
  *
  * Return: 1 on success, -1 on failure.
  */
@@ -18,9 +20,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
-		return -1;
+		return (-1);
+
 	if (text_content != NULL)
 	{
 
