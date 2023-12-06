e2059d7e108abb82146a252b87e972afd8eb2274
SuperPims <indexes-benzine0p@icloud.com>
2023-12-06 14:13:18 +0100

    add the ondition if write fails or does not write the expected amount of bytes

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,10 +5,14 @@
  * @filename: Name of the file to read.
  * @letters: Number of letters to read and print.
  *
- * This function reads a specified number of letters from the file with the given
- * filename and prints them to the standard output. It returns the actual number
- * of letters read and printed. If the file cannot be opened, the function returns 0.
- * If memory allocation fails, it returns 0. If reading or printing fails, it returns 0.
+ * This function reads a specified number of letters
+ * from the file with the given
+ * filename and prints them to the standard output.
+ * It returns the actual number
+ * of letters read and printed.
+ * If the file cannot be opened, the function returns 0.
+ * If memory allocation fails, it returns 0.
+ * If reading or printing fails, it returns 0.
  *
  * Return: The number of letters read and printed, or 0 on failure.
  */
@@ -16,7 +20,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read;
+	ssize_t bytes_read, bytes_written;
 	char *buffer;
 
 	if (filename == NULL)
@@ -36,16 +40,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
-
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
 
-	buffer[bytes_read] = '\0';
-	printf("%s", buffer);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	free(buffer);
 	close(fd);
+
+	if (bytes_written == -1 || (size_t)bytes_written < letters)
+		return (0);
+
 	return (bytes_read);
 }
